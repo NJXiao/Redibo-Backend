c4d32e5e9423508856230420b19b00a936136a2e
efrain <efrainperalta.f@gmail.com>
2025-04-12 17:42:39 -0400

    feat: agregar ruta para la reserva

---
+++ src/routes/reservation.js
@@ -0,0 +1,6 @@
+import { Router } from "express";
+import { ReservationController } from "../controllers/reservation.js";
+
+export const reservationRouter = Router()
+
+reservationRouter.post("/", ReservationController.createReservation)
