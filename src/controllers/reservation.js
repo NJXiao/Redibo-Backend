2f8e1a6a9a19089b16bcd659778876d3f28c7de7
efrain <efrainperalta.f@gmail.com>
2025-04-12 17:38:34 -0400

    feat: agragar controlador para la reserva

--- src/controllers/reservation.js
+++ src/controllers/reservation.js
@@ -1,1 +1,28 @@
+import { validateReservation } from "../validations/reservation.js";
+import { ReservationModel } from "../models/reservation.js";
 
+export class ReservationController {
+  static async createReservation(req, res) {
+    try {
+      const parseResult = validateReservation(req.body)
+
+      if (!parseResult.success) {
+        return res.status(400).json({ error: JSON.parse(parseResult.error.message)})
+      }
+
+      const { userId, carId, starDate, endDate, estado } = parseResult.data
+
+      const newReservation = await ReservationModel.createReservation({ userId, carId, starDate, endDate, estado })
+      res.status(201).json(newReservation)
+
+    }  catch (error){
+      console.error(error)
+      if (error instanceof Error) {
+        res.status(400).json({ error: error.message })
+      } else {
+        res.status(500).json({ error: 'Error interno del servidor' })
+      }
+    }
+  }
+
+}
