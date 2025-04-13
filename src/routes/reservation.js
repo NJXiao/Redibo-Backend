39d68d744d43798711b1ec4655b78ce2374c8060
efrain <efrainperalta.f@gmail.com>
2025-04-13 16:17:30 -0400

    feat: Agregar rutas para actualizar el estado de una reserva y eliminar una reserva

--- src/routes/reservation.js
+++ src/routes/reservation.js
@@ -4,3 +4,5 @@ import { ReservationController } from "../controllers/reservation.js";
 export const reservationRouter = Router()
 
 reservationRouter.post('/', ReservationController.createReservation)
+reservationRouter.patch('/:id/state', ReservationController.updateReservationState)
+reservationRouter.delete('/:id', ReservationController.deleteReservation)
