7ebfbe9d1fd1f6659fb2d4a1bb994a607911bef7
Daniel Virreira <virreira.daniel@gmail.com>
2025-04-14 18:31:56 -0400

    Agregar rutas del controlador.

---
+++ src/routes/searchRoutes.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import searchController from '../controllers/searchController';
+
+const router = express.Router();
+
+//POST: Guardar
+router.post('/search-history', searchController.saveSearch);
+
+//GET: Obtener
+router.get('/search-history/:userId', searchController.getUserSearches);
+
+//DELETE: Eliminar
+router.delete('/search-history/:id', searchController.deleteSearchById);
+
+export default router;
