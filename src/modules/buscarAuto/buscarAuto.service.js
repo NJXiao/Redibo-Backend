a7c331c95a95091dbfc235173ea0ac198c7b2db8
Maria Belen Quisbert Fernandez <maria.belen.quis@gmail.com>
2025-04-15 00:17:49 -0400

    add: creacion de de endpoint para los autos

--- src/modules/buscarAuto/buscarAuto.service.js
+++ src/modules/buscarAuto/buscarAuto.service.js
@@ -1,7 +1,51 @@
 const prisma = require('../../config/prisma');
 
-const findAll = async() => {
-    return prisma.carro.findMany();
-}
+const findAll = async () => {
+  try {
+    return await prisma.carro.findMany({
+      select: {
+        id: true,
+        modelo: true,
+        marca: true,
+        asientos:true,
+        puertas:true,
+        transmision:true,
+        precio_por_dia: true,
+        combustiblecarro:{
+            select:{
+            tipocombustible:{
+              select:{
+                tipo_de_combustible:true,
+              }
+            },
+          },},
+        estado:true,
+        usuario_rol:{
+            select:{
+                usuario:{
+                    select:{
+                       nombre:true,
+                    }
+                }
+            }
+        },
+        direccion: true,
+        imagen: {
+            take: 1,
+            orderBy: {
+              id: 'asc'
+            },
+            select: {
+                id: true,
+                data: true,
+                id_carro: true
+            }
+        }
+    }
+    });
+  } catch (error) {
+    console.error('Error al obtener los carros:', error);
+  }
+};
 
-module.exports ={ findAll };
+module.exports = { findAll };
