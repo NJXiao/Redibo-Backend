a765608690e52872f7d4f02e577899c2ed7c4f56
poko233 <victorterrazasc05@gmail.com>
2025-05-16 14:08:03 -0400

    bug /login-failed

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -16,7 +16,7 @@ router.get(
   "/google/callback",
   passport.authenticate("google", {
     session: false,
-    failureRedirect: "/login-failed",
+    failureRedirect: "/login",
   }),
   userController.googleCallback
 );
